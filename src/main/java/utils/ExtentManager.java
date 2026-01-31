e0911e7863cd5cd70f8deca16089959042c8c765
getbhavin84 <getbhavin84@gmail.com>
2026-02-01 01:02:10 +0530

    Update ExtentManager and reports

--- src/main/java/utils/ExtentManager.java
+++ src/main/java/utils/ExtentManager.java
@@ -1,19 +1,32 @@
 package utils;
 
-import com.aventstack.extentreports.*;
+import java.io.File;
+
+import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentManager {
 
-    private static ExtentReports extent;
+	private static ExtentReports extent;
 
-    public static ExtentReports getInstance() {
-        if (extent == null) {
-            ExtentSparkReporter spark =
-                new ExtentSparkReporter("reports/ExtentReport.html");
-            extent = new ExtentReports();
-            extent.attachReporter(spark);
-        }
-        return extent;
-    }
+	public static ExtentReports getInstance() {
+		if (extent == null) {
+
+			String reportPath = System.getProperty("user.dir") + File.separator
+					+ "reports" + File.separator + "ExtentReport.html";
+
+			ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
+
+			spark.config().setOfflineMode(true);
+
+			spark.config().setTheme(Theme.DARK);
+			spark.config().setDocumentTitle("Automation Report");
+			spark.config().setReportName("Selenium Test Results");
+
+			extent = new ExtentReports();
+			extent.attachReporter(spark);
+		}
+		return extent;
+	}
 }
